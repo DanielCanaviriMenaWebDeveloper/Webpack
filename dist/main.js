5dbe5f5f0502fd84682d23c345559bd436f9597f
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2021-02-07 20:51:49 -0400

    Modos de Webpack

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,15 @@
-console.log(50);
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+eval("\r\nlet a = 20;\r\nlet b = 30;\r\n\r\nlet suma = a + b;\r\n\r\nconsole.log( suma );\r\n\r\n\n\n//# sourceURL=webpack://configuracion_webpack/./src/index.js?");
+/******/ })()
+;
